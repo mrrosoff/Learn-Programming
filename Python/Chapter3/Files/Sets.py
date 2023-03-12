f430703da0b9cf3add467bbbd9bbc0fa572c3875
Max Rosoff <me@maxrosoff.com>
2023-03-12 13:53:06 -0700

    Update Sets.py

--- Python/Chapter3/Files/Sets.py
+++ Python/Chapter3/Files/Sets.py
@@ -1,9 +1,10 @@
 
-# Sets are like lists... Except they cant be indexed.
+# Sets are like lists... Except they cant be indexed. The reason for this is
+# sets are internally sorted and contain only 1 of each item.
 
 if __name__ == "__main__":
 
-    aSet = {1, 3, 7}
+    aSet = {1, 3, 7, 7}
     anotherSet = {"This", "is", "a", "set!"}
 
     print(aSet, anotherSet)
@@ -12,4 +13,5 @@ if __name__ == "__main__":
 
     # print(aSet[1])
 
-    # But sets dont have indexes... Sets are worthless... Use lists...
+    # But sets dont have indexes... Sets are for the most part worthless unless you 
+    # need the "only 1 of each item property of them". Use lists instead.
